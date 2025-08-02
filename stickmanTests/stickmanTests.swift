eec0e4227e5a262c2ad3c8b0ea7ca210012de91c
Reann <reann.ng@students.sota.edu.sg>
2025-08-02 10:03:35 +0800

    Initial Commit

---
+++ stickmanTests/stickmanTests.swift
@@ -0,0 +1,17 @@
+//
+//  stickmanTests.swift
+//  stickmanTests
+//
+//  Created by .. on 2/8/25.
+//
+
+import Testing
+@testable import stickman
+
+struct stickmanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
